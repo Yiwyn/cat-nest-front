670558259ef8f914ea088cf85278a81f9ee563bc
Yiwyn <5597104@qq.com>
2023-08-14 22:58:31 +0800

    demo example 编写

--- src/pages/Example.tsx
+++ src/pages/Example.tsx
@@ -2,13 +2,30 @@ import {Button, Input, message} from "antd";
 import {useState} from "react";
 
 const {TextArea} = Input
+
 // 案例文件
+
+
+class User {
+    id: string
+    name: string
+
+    constructor(id: string, name: string) {
+        this.id = id
+        this.name = name
+    }
+}
+
+
 export default function Example() {
 
     const [messageApi, contextHolder] = message.useMessage()
 
     const [context, setContext] = useState("")
 
+    const [infos, setInfos] = useState([
+        new User("1", "Yiwyn"), new User("2", "Cat"), new User("3", "Cortana")
+    ])
 
     const showInfo = (message: string) => {
         messageApi.info(message)
@@ -18,6 +35,21 @@ export default function Example() {
         console.log(context)
         setContext("")
     }
+
+    const addItem = () => {
+        const id = (infos.length + 1).toString()
+        const u = new User(id, "随机添加" + id)
+        infos.push(u)
+        const us = [...infos]
+        setInfos(us)
+        console.log(infos)
+    }
+
+    const infoRender = infos.map(u => (
+        <li key={u.id}>
+            {u.name}
+        </li>
+    ));
 
     return (
         <>
@@ -40,6 +72,16 @@ export default function Example() {
             <Button type={"primary"} onClick={() => showTextArea()}>
                 弹出内容
             </Button>
+
+
+            {/*    列表渲染*/}
+            <ul>
+                {infoRender}
+            </ul>
+            <Button onClick={() => addItem()}>
+                添加一个随机元素
+            </Button>
+
         </>
     )
 
